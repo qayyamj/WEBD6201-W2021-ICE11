b25ea7a555b081d1bbf82e496550d9f261639838
qayyamj <qayyamj@gmail.com>
2021-04-10 21:09:46 -0400

    switched to MongoDB Atlas

--- Server/Config/db.js
+++ Server/Config/db.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Secret = exports.URI = void 0;
-exports.URI = "mongodb://localhost/webd6201";
+exports.Host = exports.Secret = exports.RemoteURI = exports.LocalURI = void 0;
+exports.LocalURI = "mongodb://localhost/webd6201";
+exports.RemoteURI = "mongodb+srv://qayyamj:nQgmnPwvGtD3PX3R@webd6201.1qnhc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 exports.Secret = "someSecret";
+exports.Host = "MongoDB Atlas";
 //# sourceMappingURL=db.js.map
--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -1,7 +1,4 @@
-export const URI = "mongodb://localhost/webd6201";
+export const LocalURI = "mongodb://localhost/webd6201";
+export const RemoteURI = "mongodb+srv://qayyamj:nQgmnPwvGtD3PX3R@webd6201.1qnhc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 export const Secret = "someSecret";
-
-/* module.exports = {
-    Path: mongoDBPath,
-    Secret: sessionSecret
-}  */
+export const Host = "MongoDB Atlas"
